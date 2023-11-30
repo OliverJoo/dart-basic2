e7dccb20595e7ee80fdf8e7c820b507392cf7752
OliverJoo <jjaemoon@nate.com>
2023-11-30 17:40:50 +0900

    add Cleric class method conditions

--- lib/23_11_30/00_dart_practice.dart
+++ lib/23_11_30/00_dart_practice.dart
@@ -10,15 +10,21 @@ class Cleric {
   Cleric(this.name, this.hp, this.mp);
 
   void selfAid() {
-    // if(mp > 5){
-    mp -= 5;
-    // }
-
-    hp = maxHp;
+    if (mp >= 5) {
+      mp -= 5;
+      hp = maxHp;
+    } else {
+      print('Not enough MP! do pray~');
+    }
   }
 
   int pray(int sec) {
     mp += sec + Random().nextInt(3);
+
+    if (mp > maxMp) {
+      mp = maxMp;
+    }
+
     return mp;
   }
 
@@ -30,8 +36,25 @@ class Cleric {
 void main() {
   final cleric = new Cleric('test', 30, 10);
   cleric.printStatus();
-  print(cleric.pray(4));
+  print('After pray current MP : ${cleric.pray(4)}');
   cleric.printStatus();
   cleric.selfAid();
   cleric.printStatus();
+  cleric.selfAid();
+  cleric.printStatus();
+  cleric.selfAid();
+  cleric.printStatus();
+  print('After pray current MP : ${cleric.pray(5)}');
+  cleric.printStatus();
+  cleric.selfAid();
+  cleric.printStatus();
+  cleric.selfAid();
+  cleric.printStatus();
+  cleric.selfAid();
+  print('After pray current MP : ${cleric.pray(3)}');
+  cleric.printStatus();
+  print('After pray current MP : ${cleric.pray(3)}');
+  cleric.selfAid();
+  cleric.printStatus();
+  cleric.selfAid();
 }
