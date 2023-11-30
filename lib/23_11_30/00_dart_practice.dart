b39912102ea497ca5225f1f2e08583cd04572aec
OliverJoo <jjaemoon@nate.com>
2023-11-30 17:08:10 +0900

    add Cleric class - method : pray()

--- lib/23_11_30/00_dart_practice.dart
+++ lib/23_11_30/00_dart_practice.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric {
   String name;
   int hp;
@@ -8,7 +10,31 @@ class Cleric {
   Cleric(this.name, this.hp, this.mp);
 
   void selfAid() {
+    // if(mp > 5){
     mp -= 5;
+    // }
+
     hp = maxHp;
   }
+
+  int pray(int sec){
+    mp += sec + Random().nextInt(3);
+    return mp;
+  }
+
+  void printStatus(){
+    print('Cleric Name : $name, HP : $hp, MP : $mp ');
+  }
+}
+
+void main(){
+  final cleric = new Cleric('test', 30, 10);
+  cleric.printStatus();
+  print(cleric.pray(4));
+
+  cleric.printStatus();
+  cleric.selfAid();
+  cleric.printStatus();
+
+
 }
