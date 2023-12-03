c7faa7df6b1442f90ad568698bbcbed739a62ad3
OliverJoo <jjaemoon@nate.com>
2023-12-03 12:49:16 +0900

    231201 2-2 연습문제 설명 주석 수정

--- lib/23_12_01/cleric_main.dart
+++ lib/23_12_01/cleric_main.dart
@@ -1,18 +1,22 @@
 import 'package:untitled/23_12_01/Cleric.dart';
 
 void main() {
-  // A: 이 클래스는 Cleric(“아서스", hp: 40, mp: 5) 와 같이, 이름, HP, MP 를 지정하여 인스턴스화 할 수 있다
+  // A: Cleric(“아서스", hp: 40, mp: 5) 처럼 이름, HP, MP 를 지정하여 인스턴스화 할 수 있다
   final cleric = Cleric('아서스', hp: 40, mp: 5);
-  // B: 이 클래스는 Cleric(“아서스", hp: 35) 와 같이, 이름과 HP만으로 지정하여 인스턴스화 할 수 있다. 이 때, MP는 최대 MP와 같은 값이 초기화 된다
+  // B: Cleric(“아서스", hp: 35) 처럼, 이름과 HP만으로 지정하여 인스턴스화 가능.
+  // 이 때, MP는 최대 MP와 같은 값이 초기화 된다
   final cleric1 = Cleric('아서스', hp: 35); // B
-  // C: 이 클래스는 Cleric(“아서스") 와 같이 이름만을 지정하여 인스턴스화 할 수 있다. 이 때, HP 와 MP 는 최대 HP와 최대 MP로 초기화 된다
+  // C: Cleric(“아서스") 처럼, 이름만 지정하여 인스턴스화 가능. 이때, HP와 MP는 각각 최대치로 초기화
   final cleric2 = Cleric('아서스');
-  // D: 이 클래스는 Cleric() 과 같이 이름을 지정하지 않는 경우에는 인스턴스화 할 수 없다고 한다. (이름이 없는 성직자는 존재 할 수 없음)
+  // D: Cleric() 과 같이 이름을 지정하지 않는 경우에는 인스턴스화 할 수 없다
   // final cleric3 = Cleric();
 
   cleric.printStatus();
   cleric1.printStatus();
   cleric2.printStatus();
+
+  final test2 = const [];
+  var test1 = const [];
 
   // cleric.printStatus();
   // print('Recovered MP : ${cleric.pray(4)}');
