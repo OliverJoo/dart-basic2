00459619519e7a9d4a1e09a1b4980748282a7cc8
OliverJoo <jjaemoon@nate.com>
2023-12-05 16:19:39 +0900

    연습문제 12-4 제출

---
+++ lib/23_12_05/asset_management.dart
@@ -0,0 +1,80 @@
+// 연습문제 12-2
+// 가
+abstract class Asset {
+  String name;
+  int price;
+
+  Asset({
+    required this.name,
+    required this.price,
+  });
+}
+
+abstract interface class Thing {
+  set weight(double weight);
+
+  double get weight;
+}
+
+// 나
+abstract class IntangibleAsset extends Asset {
+  String right; // 무형자산의 권리명
+
+  IntangibleAsset(
+      {required this.right, required super.name, required super.price});
+}
+
+// 다
+class Patent extends IntangibleAsset {
+  int rightPeriod; // 무형자산 특허 기간
+
+  Patent(
+      {required this.rightPeriod,
+      required super.right,
+      required super.name,
+      required super.price});
+}
+
+abstract class TangibleAsset extends Asset implements Thing {
+  double _weight;
+
+  TangibleAsset({
+    required super.name,
+    required double weight,
+    required super.price,
+  }) : _weight = weight;
+
+  @override
+  double get weight => _weight;
+
+  @override
+  set weight(double weight) {
+    _weight = weight;
+  }
+}
+
+class Book extends TangibleAsset {
+  String color;
+  String isbn;
+
+  Book({
+    required this.color,
+    required this.isbn,
+    required super.name,
+    required super.price,
+    required super.weight,
+  });
+}
+
+class Computer extends TangibleAsset {
+  String color;
+  String makerName;
+
+  Computer({
+    required this.color,
+    required this.makerName,
+    required super.name,
+    required super.price,
+    required super.weight,
+  });
+}
