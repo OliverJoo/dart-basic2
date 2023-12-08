838a63eb9758d3e58b7a6072ae74b74c295e608f
OliverJoo <jjaemoon@nate.com>
2023-12-08 14:42:48 +0900

    문자열조작 실습2 제출

--- lib/23_12_08/dart_string.dart
+++ lib/23_12_08/dart_string.dart
@@ -2,19 +2,22 @@ class Word {
   String word = 'teEVX';
 
   bool isVowel(int i) {
-    final List<String> strList = [
-      'a',
-      'e',
-      'i',
-      'o',
-      'u',
-      'A',
-      'E',
-      'I',
-      'O',
-      'U'
-    ];
+    final List<String> strList = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' ];
 
     return strList.contains(word.substring(i, i + 1));
+  }
+
+  bool isConsonant(int i) {
+    return !isVowel(i);
+  }
+}
+
+void main() {
+  final wordChk = Word();
+
+  for (int i = 0; i < wordChk.word.length; i++) {
+    String chkWord = wordChk.word.substring(i, i+1);
+    print('$chkWord is Vowel : ${wordChk.isVowel(i)}');
+    print('$chkWord is Consonant : ${wordChk.isConsonant(i)}');
   }
 }
