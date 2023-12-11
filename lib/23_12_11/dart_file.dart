3370af9a29840138097678c77dc7fa30f1ed48af
OliverJoo <jjaemoon@nate.com>
2023-12-11 14:45:42 +0900

    231211 연습문제 1 Dart 파일 조작 제출

---
+++ lib/23_12_11/dart_file.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+
+void copy(String source, String target) {
+  final sourceFile = File(source);
+  final targetFile = File(target);
+
+  targetFile.writeAsStringSync(sourceFile.readAsStringSync());
+}
+
+void main() {
+  final file = File('source.txt');
+  // file.writeAsStringSync('Source file is here!');
+
+  print(file.readAsStringSync());
+  copy('source.txt', 'target.txt');
+}
