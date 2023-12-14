cc124eacba56c55bb507d16411d1322976e7f209
OliverJoo <jjaemoon@nate.com>
2023-12-14 15:40:05 +0900

    Dart 네트워크 통신 연습문제 1 제출 - 파일명 변경

--- lib/23_12_14/dart_network_01.dart
+++ lib/23_12_14/dart_network_01.dart
@@ -1,7 +1,7 @@
 import "dart:convert";
 import "package:http/http.dart" as http;
 
-import "MoiveInfo.dart";
+import "MovieInfo.dart";
 
 void main() async {
   final moveiInfo = MoiveInfo.fromJson(await getMovieInfo());
