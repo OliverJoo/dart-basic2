c782b7e9c4104d6685392d69429fe43b30e0b8e0
OliverJoo <jjaemoon@nate.com>
2023-12-14 16:42:36 +0900

    Dart 네트워크 통신 연습문제 4 제출

--- lib/23_12_14/dart_network_03.dart
+++ lib/23_12_14/dart_network_03.dart
@@ -7,7 +7,20 @@ final String iconFileName = 'icon.ico';
 final String imageUrl = 'https://alimipro.com/favicon.ico';
 
 void main() async {
-  saveFile(await downloadImage(imageUrl), iconFileName);
+  final stopwatch = Stopwatch();
+
+  print('다운로드 시작');
+
+  stopwatch.start();
+  saveFile(await downloadImage(imageUrl), iconFileName)
+      .then((value) => stopwatch.stop());
+
+  print('다운로드 끝');
+  print('===========');
+  print('소요시간 : ${stopwatch.elapsed}');
+
+  final byte = (await File(iconFileName).readAsBytes()).length;
+  print('용량 $byte bytes');
 }
 
 Future<File> saveFile(Uint8List bytes, String fileName) async {
@@ -16,7 +29,6 @@ Future<File> saveFile(Uint8List bytes, String fileName) async {
 
 Future<Uint8List> downloadImage(String url) async {
   final response = await http.get(Uri.parse(url));
-
   if (response.statusCode == 200) {
     return response.bodyBytes;
   } else {
