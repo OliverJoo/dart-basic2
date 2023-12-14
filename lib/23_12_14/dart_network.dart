01c2fa08f37d2e4d52b1550a1749c6809d6cbf84
OliverJoo <jjaemoon@nate.com>
2023-12-14 15:20:01 +0900

    Dart 네트워크 통신 연습문제 1 제출

---
+++ lib/23_12_14/dart_network.dart
@@ -0,0 +1,17 @@
+import "dart:convert";
+import "package:http/http.dart" as http;
+
+void main() async {
+  print(jsonEncode(await getMovieInfo()));
+}
+
+Future<Map<String, dynamic>> getMovieInfo() async {
+  final response = await http.get(Uri.parse(
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=a64533e7ece6c72731da47c9c8bc691f&language=ko-KR&page=1'));
+
+  if (response.statusCode == 200) {
+    return jsonDecode(response.body) as Map<String, dynamic>;
+  } else {
+    throw Exception('Failed to load Movie Information');
+  }
+}
