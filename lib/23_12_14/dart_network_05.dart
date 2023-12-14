dd1a3f604ff9ce225241e240b74f29622e88077a
OliverJoo <jjaemoon@nate.com>
2023-12-14 17:35:00 +0900

    Dart 네트워크 통신 연습문제 5 병렬처리 제출

---
+++ lib/23_12_14/dart_network_05.dart
@@ -0,0 +1,58 @@
+import "dart:io";
+import "dart:typed_data";
+import "package:http/http.dart" as http;
+
+final String imageUrl1 = 'https://en.wikipedia.org/wiki/File:Image_created_with_a_mobile_phone.png';
+final String imageUrl2 = 'https://upload.wikimedia.org/wikipedia/commons/a/a8/TEIDE.JPG';
+final String imageUrl3 = 'https://en.wikipedia.org/wiki/Image#/media/File:Pencil_drawing_of_a_girl_in_ecstasy.jpg';
+
+final String imageFileName1 = 'Image_created_with_a_mobile_phone.png';
+final String imageFileName2 = 'TEIDE.JPG';
+final String imageFileName3 = 'Pencil_drawing_of_a_girl_in_ecstasy.jpg';
+
+void main() async {
+  final stopwatch = Stopwatch();
+
+  print('다운로드 시작');
+
+  stopwatch.start();
+  downloadImageFiles().then((value) => stopwatch.stop());
+  print('다운로드 끝');
+  print('===========');
+  print('순차처리 소요시간 : ${stopwatch.elapsed}');
+  stopwatch.reset();
+
+  stopwatch.start();
+  downloadImageFilesParallel().then((value) => stopwatch.stop());
+
+  print('다운로드 끝');
+  print('===========');
+  print('병렬처리 소요시간 : ${stopwatch.elapsed}');
+}
+
+// 3개의 이미지 순차처리
+Future<void> downloadImageFiles() async {
+  List<File> saveFiles = [
+    await downloadImage(imageUrl1, imageFileName1),
+    await downloadImage(imageUrl2, imageFileName2),
+    await downloadImage(imageUrl3, imageFileName3),
+  ];
+}
+
+// 3개의 이미지 병렬처리
+Future<void> downloadImageFilesParallel() async {
+  List<Future<File>> saveFiles = [
+    downloadImage(imageUrl1, imageFileName1),
+    downloadImage(imageUrl2, imageFileName2),
+    downloadImage(imageUrl3, imageFileName3),
+  ];
+}
+
+Future<File> downloadImage(String url, String fileName) async {
+  final response = await http.get(Uri.parse(url));
+  if (response.statusCode == 200) {
+    return File(fileName).writeAsBytes(response.bodyBytes);
+  } else {
+    throw Exception('Failed to load Movie Information');
+  }
+}
